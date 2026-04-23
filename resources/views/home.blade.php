794af67d8ef2b60fe526759d0ef85ab7cebff535
marcoalq9 <marcolopezquesada15@gmail.com>
2026-04-22 23:09:49 -0600

    feat: vista home de prueba con layout maestro

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Inicio - Flotilla')
+
+@section('page_title', 'Dashboard')
+
+@section('content')
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">¡Bienvenido al Sistema de Flotilla!</h3>
+        </div>
+        <div class="card-body">
+          <p>El layout maestro funciona correctamente.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
